64cc9beb731aa37582d2d2631d7e71a28d2d5391
bezabu <>
2024-01-10 17:00:50 +0000

    Add feedback for form

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 def modulo(num, val):
@@ -8,6 +10,7 @@ def modulo(num, val):
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -34,8 +37,11 @@ def about_member(member_name):
     return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
